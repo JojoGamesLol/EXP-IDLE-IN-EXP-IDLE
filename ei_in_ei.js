7376ff8d356976e04b1aaed1e316dd9d912fb331
JojoGamesLol <143434175+JojoGamesLol@users.noreply.github.com>
2023-09-07 08:17:54 -0300

    Update ei_in_ei.js

--- ei_in_ei.js
+++ ei_in_ei.js
@@ -1,1 +1,11 @@
+import { ExponentialCost, FirstFreeCost, LinearCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { parseBigNumber, BigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
 
+var id = "ei_in_ei";
+var name = "EXP IDLE IN EXP IDLE";
+var description = "No Theory description.";
+var authors = "JojoGames320";
+var version = "1.0.0";
