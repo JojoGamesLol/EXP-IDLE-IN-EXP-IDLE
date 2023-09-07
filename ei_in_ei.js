0769855fc8fb9c8312fc0b9e885e42c6f26bc6ad
JojoGamesLol <143434175+JojoGamesLol@users.noreply.github.com>
2023-09-07 11:28:32 -0300

    Update ei_in_ei.js

--- ei_in_ei.js
+++ ei_in_ei.js
@@ -48,7 +48,7 @@ var MainPanel = () => {
                             text: ()=> `Buy ${buyMode}`, 
                             onClicked: ()=> {buyTypeInc();}
                     }),
-                ]
+             )} ]
         ]
    })
 }
